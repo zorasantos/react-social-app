c8a3148531862c2b27a4d05e428b2056b6c55400
Zoranildo Santos <zoranildo22@gmail.com>
2020-03-21 20:55:02 -0300

    FBAuth

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,7 @@ var firebaseConfig = {
     storageBucket: "react-social-app-248c7.appspot.com",
     messagingSenderId: "31290543404",
     appId: "1:31290543404:web:6219122470a52215f5b089"
-  }
+}
   
 
 const firebase = require('firebase')
@@ -45,13 +45,41 @@ app.get('/screams', (req, res) => {
         res.status(500).json({ error: err.code })
     })
 })
-app.post('/scream', (req, res) => {
+
+const FBAuth = (req, res, next) => {
+    let idToken
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1]
+    } else {
+        console.error('Token não encontrado!')
+        return res.status(403).json({ error: 'Não autorizado!' })
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken
+            console.log(decodedToken)
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get()
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle
+            return next()
+        })
+        .catch(err => {
+            console.error('Erro ao verificar o token!', err)
+            return res.status(403).json(err)
+        })
+}
+app.post('/scream', FBAuth, (req, res) => {
     if (req.body.body.trim() === '') {
         return res.status(400).json({ body: 'Esse campo não pode estar vazio!' })
     }
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     }
 
@@ -66,6 +94,16 @@ app.post('/scream', (req, res) => {
             console.error(err)
         })
 })
+const isEmail = (email) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(email.match(regEx)) return true
+    else return false
+}
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true
+    else return false
+}
 // Signup route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -74,8 +112,24 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     }
+
+    let errors = {}
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Campo obrigatorio!'
+    } else if(!isEmail(newUser.email)) {
+        errors.email = 'Você deve inserir um email valido!'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Campo obrigatorio!'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'As senhas não conferem!'
+    if(isEmpty(newUser.handle)) errors.handle = 'Campo obrigatorio!'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+
     //TODO: validate data
-    let token, userId;
+    let token, userId
     db.doc(`/users/${newUser.handle}`)
       .get()
       .then( doc => {
@@ -112,6 +166,36 @@ app.post('/signup', (req, res) => {
               return res.status(500).json({ error: err.code })
           }
       })
+})
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let errors = {}
+    if(isEmpty(user.email)) errors.email = 'Campo obrigatorio!'
+    if(isEmpty(user.password)) errors.password = 'Campo obrigatorio!'
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({ token })
+        })
+        .catch(err => {
+            console.error(err)
+            if(err.code === 'auth/wrong-password') {
+                return res.status(403).json({ general: 'Senha invalida, tente novamente!' })
+            } else {
+                return res.status(500).json({ error: err.code })
+            }
+        })
 })
 
 exports.api = functions.region('europe-west1').https.onRequest(app)
