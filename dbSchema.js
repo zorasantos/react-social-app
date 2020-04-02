6bc334fb509a552440df996bddf9f3faa708d42c
zoranildosantos <zoranildo22@gmail.com>
2020-04-02 09:34:52 -0300

    Editor config

--- dbSchema.js
+++ dbSchema.js
@@ -1,11 +1,49 @@
 let db = {
-    screams: [
-        {
-            userHandle: 'user',
-            body: 'this is the scream body',
-            createdAt: '2020-03-19T14:16:49.064Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ]
+  users: [
+    {
+      userId: '785hgoeyw46453jfbiw4twwf8w33b',
+      email: 'zora@gmail.com',
+      handle: 'zora',
+      createdAt: '2020-03-19T14:16:49.064Z',
+      imageUrl: 'image/kjdkvjdksssgd/akjbfkad',
+      bio: 'Ola, meu nome e zora, prezer em conhecer você.',
+      website: 'https://user.com',
+      location: 'London, UK'
+
+    }
+  ],
+
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-03-19T14:16:49.064Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
+
+const userDetails = {
+  //Redux data
+  credentials: {
+    userId: '784bf87efb64nsg485h5n585nfwebedbsdu',
+    email: 'user@gmail.com',
+    handle: 'user',
+    createdAt: '2020-03-19T14:16:49.064Z',
+    imageUrl: 'image/sgsg/dgsg',
+    bio: 'Oi, eu sou o Zora',
+    website: 'https://user.com',
+    location: 'London, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'dbkfjdhkdjdjhfjh'
+    },
+    {
+      userHandle: 'user',
+      screamId: '84bg984t3bg98u'
+    }
+  ]
 }
