0d7c42dc3a87afd3a25160b5d38f33a7d90c5fc0
zoranildosantos <zoranildo22@gmail.com>
2020-04-03 22:09:53 -0300

    Frontend initial

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link'
+
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <AppBar>
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/signup">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+}
+
+export default Navbar
